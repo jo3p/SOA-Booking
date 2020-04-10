a259858ad7911fcdd7c827d5cbd9419c1c9a8188
Joep Sterk <joep96@hotmail.com>
2020-04-11 00:12:53 +0200

    Changed port-nr

--- Booking service/app.py
+++ Booking service/app.py
@@ -8,4 +8,4 @@ api = Api(app)
 api.add_resource(PlaceBookings, '/place_booking/', methods=['POST'])
 api.add_resource(PlaceBooking, '/placed_bookings/<int:booking_id>', methods=['GET', 'PUT', 'DELETE'])
 
-app.run(host='0.0.0.0', port=5000, debug=True)
+app.run(host='0.0.0.0', port=5001, debug=True)
